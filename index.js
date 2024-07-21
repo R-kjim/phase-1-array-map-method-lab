9d66a55194e9793585ef6db4bc1e65c7dee45349
R-kjim <rkayjim@gmail.com>
2024-07-21 09:30:15 +0300

    mapping

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
+  let outputArray=[]
+  for(let i=0;i<tutorials.length;i++){
+    let tSplit=tutorials[i]
+    let tSplit1=tSplit.split(' ')
+    let newArray=[]
+    for(let j=0;j<tSplit1.length;j++){
+      let newWord=tSplit1[j]
+      let output1=newWord.charAt(0)
+      let output2=output1.toUpperCase()
+      let output=output2+newWord.slice(1)
+      newArray.push(output)
+    }
+    let conc=newArray.join(' ')
+    tutorials[i]=conc
+    
+  }
   return tutorials
 }
+console.log(titleCased())
